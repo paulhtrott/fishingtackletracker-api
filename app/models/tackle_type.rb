0655df59b4758e63ba7efaf6a07298ed134393de
Paul Trott <paultrott78@gmail.com>
2017-09-26 19:03:01 -0500

    added tests for tackle type name validation

--- app/models/tackle_type.rb
+++ app/models/tackle_type.rb
@@ -1,4 +1,6 @@
 class TackleType < ApplicationRecord
   belongs_to :tackle_category
   has_many :tackles
+
+  validates :name, uniqueness: true, presence: true
 end
--- test/models/tackle_type_test.rb
+++ test/models/tackle_type_test.rb
@@ -6,4 +6,27 @@ describe TackleType do
   it "must be valid" do
     value(tackle_type).must_be :valid?
   end
+
+  describe "validations" do
+
+    describe "name" do
+
+      it "should be present" do
+        tackle_type = TackleType.create
+        value(tackle_type.errors.messages[:name]).must_include "can't be blank"
+      end
+
+      describe "unique" do
+
+        before do
+          tackle_type
+        end
+
+        it "should be unique" do
+          new_tackle_type = TackleType.create(name: tackle_type.name)
+          value(new_tackle_type.errors.messages[:name]).must_include "has already been taken"
+        end
+      end
+    end
+  end
 end
